d8866b54cd20ab873c7b5e4cc0a7b38aab6560ae
Nicollas Lopes <collascollas29@gmail.com>
2023-08-02 17:09:10 -0300

    Criando o componente da home_page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MenuComponent } from './components/menu/menu.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
